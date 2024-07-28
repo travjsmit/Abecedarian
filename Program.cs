2445190397e43f2a29d9f56dbd22b08725dbd99c
travjsmit <inivid@icloud.com>
2024-07-28 16:39:41 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(IsAbecedarian("abdest")); // True
+        Console.WriteLine(IsAbecedarian("agnosy")); // True
+        Console.WriteLine(IsAbecedarian("biopsy")); // False
+        Console.WriteLine(IsAbecedarian("deux"));   // True
+        Console.WriteLine(IsAbecedarian("dimpsy")); // True
+    }
+
+    static bool IsAbecedarian(string word)
+    {
+        for (int i = 0; i < word.Length - 1; i++)
+        {
+            if (word[i] > word[i + 1])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
